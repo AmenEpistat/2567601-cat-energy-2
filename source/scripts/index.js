a9e351e3642bea35cdcb49d346fdfa2fd8042cee
luiza <luizaxakieva@gmail.com>
2025-06-08 20:00:54 +0300

    Starts adding adaptive grids

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+const button = document.querySelector('.main-header__toggle');
+const mainNav = document.querySelector('.main-nav');
+
+button.onclick = function(){
+  if (!button.classList.contains('main-header__toggle--closed')){
+    button.classList.add('main-header__toggle--closed');
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    button.classList.remove('main-header__toggle--closed');
+    mainNav.classList.remove('main-nav--opened');
+  }
+};
