6641d4bf7a1308b404a290c01ce21e7a4ef71dd5
AmenEpistat <AmenEpistat@users.noreply.github.com>
2025-06-08 17:01:24 +0000

    Deploying to gh-pages from @ AmenEpistat/2567601-cat-energy-2@dc1373d667a332f90f73daa1c4b5234a851bd69f 🚀

---
+++ 6/scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".main-header__toggle"),a=document.querySelector(".main-nav");e.onclick=function(){e.classList.contains("main-header__toggle--closed")?(e.classList.remove("main-header__toggle--closed"),a.classList.remove("main-nav--opened")):(e.classList.add("main-header__toggle--closed"),a.classList.add("main-nav--opened"))};
